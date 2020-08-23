7933d4d96f2bf38b3e61d1edf1e7a7f85cc27b9d
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-08-23 23:42:24 +0200

    PeekingTakeWhile::peek

--- lib.rs
+++ lib.rs
@@ -1,20 +1,26 @@
 pub trait Single: Iterator+Sized { fn single(mut self) -> Option<Self::Item> { self.next().filter(|_| self.next().is_none()) } }
 impl<I:Iterator> Single for I {}
 
-pub struct PeekingTakeWhile<'a, I:Iterator, P> { iter: &'a mut std::iter::Peekable<I>, predicate: P }
-impl<'a, I:Iterator, P: FnMut(&<I as Iterator>::Item) -> bool> Iterator for PeekingTakeWhile<'a, I, P> {
-    type Item = <I as Iterator>::Item;
-    fn next(&mut self) -> Option<Self::Item> {
+pub struct PeekingTakeWhile<'t, I:Iterator, P> { iter: &'t mut std::iter::Peekable<I>, predicate: P }
+impl<'t, I:Iterator, P: Fn(&<I as Iterator>::Item) -> bool> PeekingTakeWhile<'t, I, P> {
+	fn peek(&mut self) -> Option<&I::Item> {
         let Self{iter, predicate} = self;
-        iter.peek().filter(|x| predicate(*x))?;
-        iter.next()
+        iter.peek().filter(|x| predicate(*x))
     }
 }
-pub trait PeekableExt<'a, I:Iterator> : Iterator {
-    #[must_use] fn peeking_take_while<P:FnMut(&<Self as Iterator>::Item) -> bool>(&'a mut self, predicate: P) -> PeekingTakeWhile<'a, I, P>;
+impl<'t, I:Iterator, P: Fn(&<I as Iterator>::Item) -> bool> Iterator for PeekingTakeWhile<'t, I, P> {
+    type Item = <I as Iterator>::Item;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.peek()?;
+        self.iter.next()
+    }
 }
-impl<'a, I:Iterator> PeekableExt<'a, I> for std::iter::Peekable<I> {
-    fn peeking_take_while<P:FnMut(&<Self as Iterator>::Item) -> bool>(&'a mut self, predicate: P) -> PeekingTakeWhile<I, P> { PeekingTakeWhile{iter: self, predicate} }
+
+pub trait PeekableExt<'t, I:Iterator> : Iterator {
+    #[must_use] fn peeking_take_while<P:Fn(&<Self as Iterator>::Item) -> bool>(&'t mut self, predicate: P) -> PeekingTakeWhile<'t, I, P>;
+}
+impl<'t, I:Iterator> PeekableExt<'t, I> for std::iter::Peekable<I> {
+    fn peeking_take_while<P:Fn(&<Self as Iterator>::Item) -> bool>(&'t mut self, predicate: P) -> PeekingTakeWhile<I, P> { PeekingTakeWhile{iter: self, predicate} }
 }
 
 pub trait NthOrLast : Iterator {
